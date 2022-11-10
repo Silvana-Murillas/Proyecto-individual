780ac74489243e68f2eeaccf9da4df1376d7a293
SilvanaMurillas <silvanamurillas@gmail.com>
2022-11-10 15:40:04 -0500

    se agregaron mas acciones y modificaciones al estado global debido a los filtros y creacion de recetas

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -6,6 +6,9 @@ import axios from 'axios';
  export const CREATE_RECIPE="CREATE_RECIPE"
  export const FILTER_AZ='FILTER_AZ'
  export const FILTER_ZA='FILTER_ZA'
+ export const FILTERL_H='FILTERL_H'
+ export const FILTERH_L='FILTERH_L'
+ export const FILTERDIETS="FILTERDIETS"
 
  export const getRecipes=()=>{
     return function(dispatch){
@@ -30,6 +33,18 @@ import axios from 'axios';
  export const filterZA=()=>{
     return {type:FILTER_ZA}
  }
+ export const filterLtoH=()=>{
+   return {type:FILTERL_H}
+ }
+
+ export const filterHtoL=()=>{
+  return {type:FILTERH_L}
+ }
+
+ export const filterDiets=(diet)=>{
+   console.log(diet)
+   return {type:FILTERDIETS,payload:diet}
+ }
 
  export const getRecipebyid=(id)=>{
    return function(dispatch){
@@ -39,5 +54,22 @@ import axios from 'axios';
    }
 }
 
+export const getDiets=()=>{
+   return function(dispatch){
+       return axios.get(`http://localhost:3001/diets`)
+       .then(r=>r.data)
+       .then(data=>dispatch({type:GET_DIETS,payload:data}))
+   }
+}
+
+export const postRecipe=({ name,image,summary,healthscore,steps,diets})=>{
+ return function(dispatch){
+   return axios.post('http://localhost:3001/recipes',{name,image,summary,healthscore,steps,diets:Array.from(diets)})
+   .then(r=>r.data)
+   .then(data=>data)
+   .catch(error=>error.message)
+ }
+
+}
 
 
--- client/src/redux/reducer/index.js
+++ client/src/redux/reducer/index.js
@@ -1,8 +1,10 @@
-import { GET_ALL_RECIPES,GET_RECIPE_DETAIL,GET_DIETS,CREATE_RECIPE,GET_ALL_RECIPES_BYNAME,FILTER_AZ,FILTER_ZA } from "../actions";
+import { GET_ALL_RECIPES,GET_RECIPE_DETAIL,GET_DIETS,CREATE_RECIPE,GET_ALL_RECIPES_BYNAME,FILTER_AZ,FILTER_ZA,FILTERL_H,FILTERH_L,FILTERDIETS } from "../actions";
 
 const initialState={
   recipes:[],
-  recipesdetail:{}
+  staticrecipes:[],
+  recipesdetail:{},
+  diets:[]
 }
 
 const rootReducer=(state=initialState,action)=>{
@@ -10,7 +12,7 @@ const rootReducer=(state=initialState,action)=>{
   switch (action.type){
     case GET_ALL_RECIPES:
         
-        return({...state,recipes:action.payload});
+        return({...state,recipes:action.payload,staticrecipes:action.payload});
 
     case GET_ALL_RECIPES_BYNAME:
         return ({...state,recipes:action.payload});
@@ -47,8 +49,59 @@ const rootReducer=(state=initialState,action)=>{
 
         return ({...state,recipes:filter});
 
-        case GET_RECIPE_DETAIL:
-          return ({...state,recipesdetail:action.payload});
+    case GET_RECIPE_DETAIL:
+      return ({...state,recipesdetail:action.payload});
+    
+    case FILTERL_H:
+      let filterltoh=[...state.recipes]
+
+      filterltoh=filterltoh.sort(
+        function(a, b){
+          if (a.healthScore > b.healthScore){
+            return 1
+          }
+          if (a.healthScore  <  b.healthScore){
+            return -1
+          }
+          return 0
+        })
+
+        return ({...state,recipes:filterltoh});
+
+    case FILTERH_L:
+      let filterhtol=[...state.recipes]
+
+      filterhtol=filterhtol.sort(
+        function(a, b){
+          if (a.healthScore < b.healthScore){
+            return 1
+          }
+          if (a.healthScore  >  b.healthScore){
+            return -1
+          }
+          return 0
+        })
+
+        return ({...state,recipes:filterhtol});
+
+    case GET_DIETS:
+      return ({...state,diets:action.payload})
+
+    case FILTERDIETS:
+      console.log (action.payload)
+      return ({...state,recipes:state.staticrecipes.filter(r=>{
+         return r.diets.filter(d=>
+          {
+             return d.hasOwnProperty('name')?d.name===action.payload:d===action.payload
+        }).length?true:false
+       
+      })})
+        
+
+  
+
+
+    
       
 
     
