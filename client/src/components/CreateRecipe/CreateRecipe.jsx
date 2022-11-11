c0c37917f25e12360c3d78c50dbea8b9f0ccae73
SilvanaMurillas <silvanamurillas@gmail.com>
2022-11-11 18:32:22 -0500

    se realizo formulario controlado

--- client/src/components/CreateRecipe/CreateRecipe.jsx
+++ client/src/components/CreateRecipe/CreateRecipe.jsx
@@ -1,18 +1,42 @@
 import React from "react";
 import {useDispatch,useSelector} from 'react-redux'
 import * as actions from "../../redux/actions/index"
+import { NavLink } from "react-router-dom";
+
+export function validate(input){
+    console.log(input)
+    let error={};
+    if(!input.name){
+       error.name='Name is required';
+    }
+    if (input.name.length>25){
+        error.name="Name must have maximum 25 characteres";
+    }
+    if(!input.summary){
+        error.summary="Summary is required";
+    }
+    if(!/https?:\/\/[\w\-]+(\.[\w\-]+)+[/#?]?.*$/.test(input.image)){
+        error.image="URL invalid, must be a url";
+    }
+    if (input.healthScore<0||input.healthScore>100){
+        error.healthScore="HealthScore must be between 0-100";
+    }
+    console.log(error)
+    return error;
+}
 
 const CreateRecipe=()=>{
     const dispatch=useDispatch();
 
     const loadedDiets=useSelector(state=>state.diets)
     
+    const [errors,seterrors]=React.useState({});
 
     const [inputRecipe,setInputRecipe]=React.useState({
         name:'',
-        image:'',
+        image:null,
         summary:'',
-        healthsScore:0,
+        healthScore:0,
         steps:'',
         diets:new Set(),   
 
@@ -35,6 +59,7 @@ const CreateRecipe=()=>{
             setInputRecipe({...inputRecipe, diets : newDiets})
            
         } else {
+            seterrors(validate({...inputRecipe,[e.target.name]:e.target.value}))
             setInputRecipe({...inputRecipe,[e.target.name]:e.target.value})
         }
     }
@@ -44,20 +69,28 @@ const CreateRecipe=()=>{
         dispatch(actions.postRecipe(inputRecipe))
         alert("New recipe added successfully")
     }
+    
 
 
     return(
         <div>
+
+<NavLink to='/home'>
+            <button type="submit" onClick={(e)=>{dispatch(actions.getRecipes())}}>Volver</button>
+          </NavLink>
             <form onSubmit={handlerOnSubmit}>
                 <label> Recipe Image: <input type="url" name='image' value={inputRecipe.image} onChange={handlerOnchange}></input></label>
+                {errors.image&&<p>{errors.image}</p>}
                 <label> Recipe Name: <input type="text" name="name" value={inputRecipe.name} onChange={handlerOnchange}></input></label>
+                {errors.name&&<p>{errors.name}</p>}
                 <label> Summary:<textarea type="text" size="255" name="summary" value={inputRecipe.summary} onChange={handlerOnchange}></textarea></label>
-
-                <label> Health Score {inputRecipe.healthscore}:<input type="range" max="100" min="0" name= "healthScore" value={inputRecipe.healthscore} onChange={handlerOnchange}></input></label>
+                {errors.summary&&<p>{errors.summary}</p>}
+                <label> Health Score {inputRecipe.healthScore}:<input type="range" max="100" min="0" name= "healthScore" value={inputRecipe.healthScore} onChange={handlerOnchange}></input></label>
+                {errors.healthScore&&<p>{errors.healthScore}</p>}
                 <label> Steps:<textarea type="text" name="steps" value={inputRecipe.steps} onChange={handlerOnchange}></textarea></label>
                 <h3>Types of diets</h3>
                 {loadedDiets&&loadedDiets.map((d)=><label>{d.name}<input type="checkbox" value={d.id} name="diets" onClick={(e)=>handlerOnchange(e)} ></input></label>)}
-                <input type="submit"></input>
+                <input type="submit" disabled={!inputRecipe.name||!inputRecipe.summary||errors.name||errors.summary||errors.healthScore||errors.image}></input>
             </form>
 
         </div>
