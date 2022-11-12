3c9e432511d47d810d6315dc880cc4426bf78a3b
SilvanaMurillas <silvanamurillas@gmail.com>
2022-11-12 13:47:19 -0500

    se agrego label con nombres para la carta de las recetas

--- client/src/components/RecipeCard/RecipeCard.jsx
+++ client/src/components/RecipeCard/RecipeCard.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import {useDispatch,useSelector} from 'react-redux'
 import { Link } from 'react-router-dom';
 
-const RecipeCard=({name,image,diets,id})=>{
+const RecipeCard=({name,image,diets,id,healthScore})=>{
     return (
         <div>
-            <Link to={`/home/recipe/${id}`}><h2>{name}</h2></Link>
+            <h2>Name:</h2>
+            <Link to={`/home/recipe/${id}`}><h3>{name}</h3></Link>
             <img src={image} alt={name} />
+            <h2>Health Score:</h2>
+            <h3>{healthScore}</h3>
+            <h2>Types of Diets:</h2>
             {diets.map(diet=><h3>{diet.hasOwnProperty('name')?diet.name:diet}</h3>)}
         </div>
     )
