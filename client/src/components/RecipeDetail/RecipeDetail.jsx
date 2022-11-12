cab666a7030a85324d4af1294c8384be06200e13
SilvanaMurillas <silvanamurillas@gmail.com>
2022-11-12 13:48:06 -0500

    se agrego boton de atras que no hiciera cambios en los filtros cuando el usuario le da el detalle a la receta

--- client/src/components/RecipeDetail/RecipeDetail.jsx
+++ client/src/components/RecipeDetail/RecipeDetail.jsx
@@ -1,29 +1,36 @@
 import React from "react";
 import {useDispatch,useSelector} from 'react-redux'
 import * as actions from "../../redux/actions/index"
-import { NavLink } from 'react-router-dom';
+import { NavLink, useHistory } from 'react-router-dom';
 
 const RecipeDetail=(props)=>{
     const dispatch=useDispatch();
     const recipesdetail=useSelector(state=>state.recipesdetail)
+    let history = useHistory();
 
     React.useEffect(()=>{
         dispatch(actions.getRecipebyid(props.match.params.id))
     },[dispatch])
 
     const handlerClick=(e)=>{
-        dispatch(actions.getRecipes())
+        history.push("/home");
     }
 
     return (
         <div>
         <NavLink to="/home"><button onClick={handlerClick}>Volver</button></NavLink>
+          <h1>Name recipe:</h1>
           <h2>{recipesdetail.name}</h2>
           <img src={recipesdetail.image} alt={recipesdetail.name}/>
+          <h2>Dish Type</h2>
           {recipesdetail.dishTypes&&recipesdetail.dishTypes.map(d=><h3>{d}</h3>)}
+          <h2>Health Score</h2>
           <h3>{recipesdetail.healthScore}</h3>
+          <h2>Diet Types</h2>
           {recipesdetail.diets&&recipesdetail.diets.map(d=><h3>{d.hasOwnProperty('name')?d.name:d}</h3>)}
-          <p>{recipesdetail.summary}</p>
+          <h2>Summary</h2>
+         <p>{recipesdetail.summary&&recipesdetail.summary.replace(/(<([^>]+)>)/ig,'')}</p>
+         <h2>Steps</h2>
           {recipesdetail.steps&& Array.isArray(recipesdetail.steps)? recipesdetail.steps.map(s=><h4>{`${s.number} ${s.step}`}</h4>):<p>{recipesdetail.steps}</p>}
         </div>
     )
