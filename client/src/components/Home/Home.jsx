fed053f52e980767a95442a9243d2948a7f32731
SilvanaMurillas <silvanamurillas@gmail.com>
2022-11-12 13:49:00 -0500

    se agrego una renderizacion con condicion en home que me muetras en 404 si el nombre de la receta no lo encontro

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -9,6 +9,8 @@ const Home=()=>{
     const dispatch=useDispatch();
     const recip=useSelector(state=>state.recipes)
     const page=useSelector(state=>state.page)
+
+    
     
     // debugger;
 
@@ -16,6 +18,21 @@ const Home=()=>{
     //  setPage(page)
     console.log(page)
     const [recipesbypage,setrecipesbypage]=React.useState(9)
+    const handlerOnclick=(e)=>{
+        e.preventDefault();
+        dispatch(actions.getRecipes())
+     }
+    if(recip==="name no found"){
+
+        return (
+            <div>
+                <Nav></Nav>
+                <button onClick={handlerOnclick}>refrescar</button>
+              <img src="248989-P42VE0-381.jpg"></img>
+                <h1>Name no found</h1>
+            </div>
+        )
+    }
 
     const limitRecipes=page * recipesbypage;
 
@@ -31,16 +48,13 @@ const Home=()=>{
    
     
       
-     const handlerOnclick=(e)=>{
-        e.preventDefault();
-        dispatch(actions.getRecipes())
-     }
+     
     return (
         <div>
             <Nav></Nav>
            <Pages></Pages>
             <button onClick={handlerOnclick}>refrescar</button>
-          {recipes&&recipes.map(recipe=><RecipeCard name={recipe.name} image={recipe.image}  diets={recipe.diets} key={recipe.id} id={recipe.id} ></RecipeCard>)}
+          {recipes&&recipes.map(recipe=><RecipeCard name={recipe.name} image={recipe.image}  diets={recipe.diets} key={recipe.id} id={recipe.id} healthScore={recipe.healthScore}></RecipeCard>)}
 
         </div>
     )
