d154ed49357259876dd5000f9652c2625e9f8513
SilvanaMurillas <silvanamurillas@gmail.com>
2022-11-20 11:05:13 -0500

    agregar flechas de next y preve al pagination y que el usuario sepa en que pag esta parado

--- client/src/components/Home/Pagination.jsx
+++ client/src/components/Home/Pagination.jsx
@@ -4,10 +4,11 @@ import * as actions from "../../redux/actions"
 import "./Pagination.css"
 
 
-const Pages=()=>{
+const Pages=({page})=>{
     const recipes=useSelector(state=>state.recipes)
     const dispatch=useDispatch();
     let pages=[]
+    
 
     if(recipes){
     for(let i=1;i<=Math.ceil(recipes.length/9);i++){
@@ -18,11 +19,32 @@ const Pages=()=>{
     const handlerClick=(e,p)=>{
         e.preventDefault();
         dispatch(actions.sendPage(p))
+        
+    }
+
+    const handlerPrev=(e,page)=>{
+        e.preventDefault();
+        if(page===1){
+            alert("No hay mas paginas")
+            dispatch(actions.sendPage(page))
+        }
+        dispatch(actions.sendPage(page-1))
+    }
+
+    const handlerNext=(e,page)=>{
+        if(page===pages[pages.length-1]){
+            alert("No hay mas paginas")
+            dispatch(actions.sendPage(page))
+        }
+        e.preventDefault();
+        dispatch(actions.sendPage(page+1))
     }
 
     return (
-        <div className="pag">
-          {pages&&pages.map(p=><button  onClick={(e)=>handlerClick(e,p)}>{p}</button>)}
+        <div >
+            <button className="pag" onClick={(e)=>handlerPrev(e,page)}>⋘</button>
+          {pages&&pages.map(p=><button key={p} className={p===page?"pageactual":"pag"} onClick={(e)=>handlerClick(e,p)}>{p}</button>)}
+          <button className="pag" onClick={(e)=>handlerNext(e,page)}>⋙</button>
         </div>
     )
 
