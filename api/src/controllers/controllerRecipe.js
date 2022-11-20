d7305a118b7bc8bf3ff95a6a1ec1f25331817cd6
SilvanaMurillas <silvanamurillas@gmail.com>
2022-11-20 11:06:40 -0500

    identacion y manejo errores en los controler de recipe

--- api/src/controllers/controllerRecipe.js
+++ api/src/controllers/controllerRecipe.js
@@ -1,181 +1,190 @@
-const e = require('express');
-const {Recipe,Diet}=require('../db');
-const {URL_API,API_KEY,URL_APIBYID,API_KEY1}=process.env;
-const axios = require('axios')
+const e = require("express");
+const { Recipe, Diet } = require("../db");
+const { URL_API, API_KEY, URL_APIBYID, API_KEY1 } = process.env;
+const axios = require("axios");
 const { Op } = require("sequelize");
 
+const addRecipe = async (name, image, summary, healthScore, steps, diets) => {
+  if (!name || !summary) {
+    throw new Error("Faltan Datos por ingresar");
+  }
 
-const addRecipe=async (name,image,summary,healthScore,steps,diets)=>{
- 
-
-    if(!name||!summary){
-        throw new Error('Faltan Datos por ingresar');
-    }
-
-    try{          
-        const recipe=await Recipe.create({name,image,summary,healthScore,steps});
-        if(diets&&diets.length){
-            await recipe.setDiets(diets)
-        }
-        return recipe;
-    }catch(error){
-        console.log(error.message)
-        throw new Error('Id diet invalid')
-    }   
-
-}
-
-const getBd=async(name)=>{
-    if(name){   
-        const findbyName=await Recipe.findAll({
-                where:{
-                    name:{
-                        [Op.iLike]:`%${name}%`
-                    }
-                },
-                include:{
-                    model:Diet,
-                    attributes:['name'],
-                    through:{
-                        attributes:[]
-                    }
-                }
-        });   
-        return findbyName;        
-    }
-    const recipes=await Recipe.findAll({
-        include:
-        [
-            {
-                model:Diet,
-                attributes:['name'],
-                through:{
-                            attributes:[]
-                        }
-            }
-        ]
+  try {
+    const recipe = await Recipe.create({
+      name,
+      image,
+      summary,
+      healthScore,
+      steps,
     });
-    return recipes;
-}
+    if (diets && diets.length) {
+      await recipe.setDiets(diets);
+    }
+    return recipe;
+  } catch (error) {
+    throw error;
+  }
+};
 
-const getApi=async(name)=>{
-     
-    const getInfoApi= await axios.get(`${URL_API}?apiKey=${API_KEY1}&addRecipeInformation=true&number=100`);
-    const infoapi= await getInfoApi.data.results;
-    
+const getBd = async (name) => {
+  if (name) {
+    const findbyName = await Recipe.findAll({
+      where: {
+        name: {
+          [Op.iLike]: `%${name}%`,
+        },
+      },
+      include: {
+        model: Diet,
+        attributes: ["name"],
+        through: {
+          attributes: [],
+        },
+      },
+    });
+    return findbyName;
+  }
+  const recipes = await Recipe.findAll({
+    include: [
+      {
+        model: Diet,
+        attributes: ["name"],
+        through: {
+          attributes: [],
+        },
+      },
+    ],
+  });
+  return recipes;
+};
 
-    const info=infoapi.map(e=>{
+const getApi = async (name) => {
+  const getInfoApi = await axios.get(
+    `${URL_API}?apiKey=${API_KEY1}&addRecipeInformation=true&number=10`
+  );
+  const infoapi = await getInfoApi.data.results;
 
-        let diets = e.diets;
-        if (e.vegetarian){
-            diets.push('vegetarian');
-        }
-        return{
-                id:e.id,
-                name:e.title,
-                image:e.image,
-                dishTypes:e.dishTypes,
-                summary:e.summary,
-                healthScore:e.healthScore,
-                diets:diets,
-                steps:e.analyzedInstructions.length&&e.analyzedInstructions[0].steps.map(e=>
-                {
-                    return {
-                        number:e.number,
-                        step:e.step
-                    }
-                })
-            }
-    })
-    info.forEach(rec=>rec.diets.forEach(async (d)=>await Diet.findOrCreate({where:{name:d}})))
-    if(name){
-        return info.filter(e=>e.name.toLowerCase().includes(name.toLowerCase()));        
+  const info = infoapi.map((e) => {
+    let diets = e.diets;
+    if (e.vegetarian) {
+      diets.push("vegetarian");
+    }
+    return {
+      id: e.id,
+      name: e.title,
+      image: e.image,
+      dishTypes: e.dishTypes,
+      summary: e.summary,
+      healthScore: e.healthScore,
+      diets: diets,
+      steps:
+        e.analyzedInstructions.length &&
+        e.analyzedInstructions[0].steps.map((e) => {
+          return {
+            number: e.number,
+            step: e.step,
+          };
+        }),
+    };
+  });
+  info.forEach((rec) =>
+    rec.diets.forEach(
+      async (d) => await Diet.findOrCreate({ where: { name: d } })
+    )
+  );
+  if (name) {
+    return info.filter((e) =>
+      e.name.toLowerCase().includes(name.toLowerCase())
+    );
+  }
+
+  return info;
+};
+
+const getRecipe = async (name) => {
+  try {
+    if (name) {
+      const nameApi = await getApi(name);
+
+      const namebD = await getBd(name);
+
+      if (!nameApi.length && !namebD.length) throw new Error("name no found");
+      const nameApi_bD = nameApi.concat(namebD);
+      return nameApi_bD;
     }
 
-   
-
-    return info;
-
-}
-
-const getRecipe=async(name)=>{
-    if(name){
-        const nameApi=await getApi(name);
-        
-        const namebD=await getBd(name);
-       
-        if(!nameApi.length&&!namebD.length)throw new Error('name no found')
-        const nameApi_bD=nameApi.concat(namebD)
-        return nameApi_bD;
-    }
-
-    const Api= await getApi();
-    const bD= await getBd();    
-    const Api_bD=Api.concat(bD);
+    const Api = await getApi();
+    const bD = await getBd();
+    const Api_bD = Api.concat(bD);
     return Api_bD;
-}
+  } catch (error) {
+    throw error;
+  }
+};
 
-const getRecipebyidfromDB=async(id)=>{
-    const findbyid=await Recipe.findByPk(id,{
-        include: [
-            {
-                model:Diet,
-                attributes:['name'],
-                through:{
-                            attributes:[]
-                        }
-            }
-        ]
-    })
-    
-    return findbyid
+const getRecipebyidfromDB = async (id) => {
+  const findbyid = await Recipe.findByPk(id, {
+    include: [
+      {
+        model: Diet,
+        attributes: ["name"],
+        through: {
+          attributes: [],
+        },
+      },
+    ],
+  });
 
-}
-const getRecipebyidfromApi=async(id)=>{
-    const getInfoApi= await axios.get(`${URL_APIBYID}${id}/information?apiKey=${API_KEY1}`);
-    const infoapi= await getInfoApi.data;
-    
-    let diets = infoapi.diets;
-    if (infoapi.vegetarian){
-        diets.push('vegetarian');
+  return findbyid;
+};
+const getRecipebyidfromApi = async (id) => {
+  const getInfoApi = await axios.get(
+    `${URL_APIBYID}${id}/information?apiKey=${API_KEY1}`
+  );
+  const infoapi = await getInfoApi.data;
+
+  let diets = infoapi.diets;
+  if (infoapi.vegetarian) {
+    diets.push("vegetarian");
+  }
+  const infobyId = {
+    id: infoapi.id,
+    name: infoapi.title,
+    image: infoapi.image,
+    dishTypes: infoapi.dishTypes,
+    summary: infoapi.summary,
+    healthScore: infoapi.healthScore,
+    diets: diets,
+    steps:
+      infoapi.analyzedInstructions.length &&
+      infoapi.analyzedInstructions[0].steps.map((e) => {
+        return {
+          number: e.number,
+          step: e.step,
+        };
+      }),
+  };
+  return infobyId;
+};
+const getRecipebyid = async (id) => {
+  if (
+    /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/.test(
+      id
+    )
+  ) {
+    try {
+      const recipebyId = await getRecipebyidfromDB(id);
+      return recipebyId;
+    } catch (error) {
+      throw error;
     }
-    const infobyId={
-        id:infoapi.id,
-        name:infoapi.title,
-        image:infoapi.image,
-        dishTypes:infoapi.dishTypes,
-        summary:infoapi.summary,
-        healthScore:infoapi.healthScore,
-        diets:diets,
-        steps:infoapi.analyzedInstructions.length&&infoapi.analyzedInstructions[0].steps.map(e=>
-            {
-                return {
-                    number:e.number,
-                    step:e.step
-                }
-            })
+  }
+  try {
+    const recipebyId = await getRecipebyidfromApi(id);
+    return recipebyId;
+  } catch (error) {
+    throw error;
+  }
+};
 
-      }
-    return infobyId;
-    
-}
-const getRecipebyid=async(id)=>{
-          
-    if(/^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/.test(id)){
-        try{
-            const recipebyId=await getRecipebyidfromDB(id)
-            return recipebyId;
-        }catch(error){
-            throw new Error ('Id recipe no found')
-        }
-    }
-    try{
-        const recipebyId= await getRecipebyidfromApi(id)
-        return recipebyId
-   }catch(error){
-      throw new Error ('Id recipe no found')
-   }
-
-}
-
-module.exports={addRecipe,getRecipe,getRecipebyid,getBd,getApi};
+module.exports = { addRecipe, getRecipe, getRecipebyid, getBd, getApi };
