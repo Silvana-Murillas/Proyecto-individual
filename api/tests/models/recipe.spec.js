77e57cf412b992451b1efbf1c29e23a4c6113be2
SilvanaMurillas <silvanamurillas@gmail.com>
2022-11-21 18:33:32 -0500

    test modelos

--- api/tests/models/recipe.spec.js
+++ api/tests/models/recipe.spec.js
@@ -1,28 +1,56 @@
-const { Recipe, conn } = require('../../src/db.js');
-const { expect } = require('chai');
+const { Recipe, conn } = require("../../src/db.js");
+const { expect } = require("chai");
+const { ValidationError } = require("sequelize");
 
-describe('Recipe model', () => {
-  before(() => conn.authenticate()
-    .catch((err) => {
-      console.error('Unable to connect to the database:', err);
-    }));
-  describe('Validators', () => {
+describe("Recipe model", () => {
+  before(() =>
+    conn.authenticate().catch((err) => {
+      console.error("Unable to connect to the database:", err);
+    })
+  );
+  describe("Validators", () => {
     beforeEach(() => Recipe.sync({ force: true }));
-    describe('name', () => {
-      it('should throw an error if name is null', (done) => {
+    describe("name", () => {
+      it("should throw an error if name and summary is null", (done) => {
         Recipe.create({})
-          .then(() => done(new Error('It requires a valid name')))
+          .then(() => done(new Error("It requires a valid name")))
           .catch(() => done());
       });
-      it('should work when its a valid name', (done) => {
-        Recipe.create({ name: 'Milanesa a la napolitana',summary:'Milanesa a la napolitana' }).then(done());
-        Recipe.findAll({where:{name:'Milanesa a la napolitana'}}).end((error,res)=>{if(error){console.log(error); done(error)}
-        else{
+      it("should work when its a valid name", async () => {
+        await Recipe.create({ name: "Frijoles", summary: "Frijoles" });
+        const res = await Recipe.findAll({ where: { name: "Frijoles" } });
+        console.log(res[0].dataValues.name);
+        expect(res[0].dataValues.name).equal("Frijoles");
+      });
+      it("should throw error if image is not valid", async () => {
+        try {
+          await Recipe.create({ name: "tira", image: "p", summary: "tira" });
+        } catch (error) {
+          expect(error).instanceOf(ValidationError);
+          expect(error.message).equal(
+            "Validation error: Validation isUrl on image failed"
+          );
+        }
+      });
+      it("should throw error if name have more than 25 characteres",async()=>{
+        try {
+          await Recipe.create({ name: "turamgtucpsgtjwtuljfndthgf", summary: "tira" });
+        } catch (error) {
+          expect(error).instanceOf(Error)
           
-          expect(res.name).equal('Milanesa a la napolitana');done()
         }
       })
-      });
+      it('should throw error if healthscore is more than 100',async()=>{
+        try {
+          await Recipe.create({ name: "turamgtucpsgtjwtuljfndthgf", summary: "tira",healthScore:105 });
+        } catch (error) {
+          expect(error).instanceOf(ValidationError)
+          expect(error.message).equal(
+            "Validation error: Validation max on healthScore failed"
+          );
+          
+        }
+      })
     });
   });
 });
