eb297cf63ccf58d98955bffa0ee24b2b9fd873e1
SilvanaMurillas <silvanamurillas@gmail.com>
2022-11-21 18:34:41 -0500

    eliminado importaciones innecesarias

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,9 @@ import { Route } from "react-router-dom";
 import Home from './components/Home/Home'
 import CreateRecipe from './components/CreateRecipe/CreateRecipe';
 import RecipeDetail from './components/RecipeDetail/RecipeDetail';
-import { NavLink } from 'react-router-dom';
-import {useDispatch,useSelector} from 'react-redux'
-import * as actions from "./redux/actions/index"
+// import { NavLink } from 'react-router-dom';
+// import {useDispatch,useSelector} from 'react-redux'
+// import * as actions from "./redux/actions/index"
 import Landing from './components/Landing_page';
 
 function App() {
