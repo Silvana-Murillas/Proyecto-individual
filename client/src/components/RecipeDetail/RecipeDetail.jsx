761be8a05c22a269e3a53346c4b86f2cdee02d62
SilvanaMurillas <silvanamurillas@gmail.com>
2022-11-23 18:40:30 -0500

    se agrego condicion en el detail para que los steps que sean un array vacio me los asigne como this recipe do not have steps

--- client/src/components/RecipeDetail/RecipeDetail.jsx
+++ client/src/components/RecipeDetail/RecipeDetail.jsx
@@ -10,7 +10,6 @@ const RecipeDetail = (props) => {
   let history = useHistory();
 
   React.useEffect(() => {
-    
     dispatch(actions.getRecipebyid(props.match.params.id));
     // eslint-disable-next-line
   }, [dispatch]);
@@ -31,35 +30,40 @@ const RecipeDetail = (props) => {
         <h1>{recipesdetail.name}</h1>
         <h2>Health Score</h2>
         <p>{recipesdetail.healthScore}</p>
-       <div className="types">
-       <div className="dish">
-        <h2>Dish Type</h2>
-          {recipesdetail.dishTypes &&
-          recipesdetail.dishTypes.map((d) => 
-          <p key={d}>{d}</p>)}
-        </div>
-        <div className="diet">
-        <h2>Diet Types</h2>
-          {recipesdetail.diets &&
-          recipesdetail.diets.map((d) => (
-          <p key={d}>{d.hasOwnProperty("name") ? d.name : d}</p>
-          ))}
-        </div>
+        <div className="types">
+          <div className="dish">
+            <h2>Dish Type</h2>
+            {recipesdetail.dishTypes &&
+              recipesdetail.dishTypes.map((d) => <p key={d}>{d}</p>)}
+          </div>
+          <div className="diet">
+            <h2>Diet Types</h2>
+            {recipesdetail.diets &&
+              recipesdetail.diets.map((d) => (
+                <p key={d}>{d.hasOwnProperty("name") ? d.name : d}</p>
+              ))}
+          </div>
         </div>
         <div className="summary">
           <h2>Summary</h2>
           <p>
-          {recipesdetail.summary &&
-            recipesdetail.summary.replace(/(<([^>]+)>)/gi, "")}
+            {recipesdetail.summary &&
+              recipesdetail.summary.replace(/(<([^>]+)>)/gi, "")}
           </p>
         </div>
         <div className="step">
           <h2>Steps</h2>
           {recipesdetail.steps && Array.isArray(recipesdetail.steps) ? (
-          recipesdetail.steps.map((s) => <p key={s.number}>{`${s.number} ${s.step}`}</p>)
+            recipesdetail.steps.length ? (
+              recipesdetail.steps.map((s) => (
+                <p key={s.number}>{`${s.number} ${s.step}`}</p>
+              ))
+            ) : (
+              <p>this recipe does not have steps</p>
+            )
           ) : (
-          <p key={recipesdetail.steps}>{recipesdetail.steps}</p>
-           )}
+            <p key={recipesdetail.steps}>{recipesdetail.steps}</p>
+          )}
         </div>
       </div>
     </div>
